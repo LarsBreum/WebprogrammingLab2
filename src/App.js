6651d5f0f4fd6b89f101c9102bc71b98dd0b2198
LarsBreum <lars.breum.lh@gmail.com>
2023-01-26 16:21:50 +0100

    added extras but no functionality

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
       <div className="continer col-12">
         <div className="row h-200 p-5 bg-light border rounded-3">
           <h2>Välj innehållet i din sallad</h2>
-          {extras.map((name) => (
+          {/* {extras.map((name) => (
             <div key={name} className="col-4">
               {name}
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
       <ComposeSalad inventory={inventory} />
--- src/ComposeSalad.js
+++ src/ComposeSalad.js
@@ -4,19 +4,74 @@ function ComposeSalad(props) {
   let foundations = Object.keys(props.inventory).filter(
     (name) => props.inventory[name].foundation
   );
+  let proteins = Object.keys(props.inventory).filter(
+    (name) => props.inventory[name].protein
+  );
+  let dressings = Object.keys(props.inventory).filter(
+    (name) => props.inventory[name].dressing
+  );
+  let extras = Object.keys(props.inventory).filter(
+    (name) => props.inventory[name].extra
+  );
+
   const [foundation, setFoundation] = useState("Pasta");
+  const [protein, setProtein] = useState("Kycklingfilé");
+  const [dressing, setDressing] = useState("Ceasardressing");
   const [extra, setExtra] = useState({ Bacon: true, Fetaost: true });
 
   return (
     <div className="container col-12">
-      <div className="row h-200 p-5 bg-light border rounded-3">
-        <h2>Välj bas</h2>
-        {foundations.map((name) => (
-          <div key={name} className="col-4">
+      <h2>Bygg din sallad</h2>
+      <form>
+        <h4>Välj Bas</h4>
+        <select
+          value={foundation}
+          onChange={(e) => {
+            setFoundation(e.target.value);
+          }}
+        >
+          {foundations.map((name) => (
+            <option value={name} key={name} className="col-4">
+              {name}
+            </option>
+          ))}
+        </select>
+        <h4>Välj Protein</h4>
+        <select
+          value={protein}
+          onChange={(e) => {
+            setProtein(e.target.value);
+          }}
+        >
+          {proteins.map((name) => (
+            <option value={name} key={name} className="col-4">
+              {name}
+            </option>
+          ))}
+        </select>
+
+        <h4>Välj tillbehör</h4>
+
+        {extras.map((name) => (
+          <label className="col-3">
+            <input type="checkbox" key={name} />
             {name}
-          </div>
+          </label>
         ))}
-      </div>
+        <h4>Välj Dressing</h4>
+        <select
+          value={dressing}
+          onChange={(e) => {
+            setDressing(e.target.value);
+          }}
+        >
+          {dressings.map((name) => (
+            <option value={name} key={name} className="col-4">
+              {name}
+            </option>
+          ))}
+        </select>
+      </form>
     </div>
   );
 }
